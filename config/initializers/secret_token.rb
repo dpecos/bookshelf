3ea3fc0ef19420495c8a9abca2280c857445a852
Daniel Pecos Martinez <contact@danielpecos.com>
2012-12-25 21:05:05 +0100

    Initial commit: rails new bookshelf --skip-test-unit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Bookshelf::Application.config.secret_token = '8db44ff18c89b88701ba3560122f89f7a6c6e1e240183b19ebfdeefe3248652cda111cfe33e912cd38576d0055d8ce39f67ad71576ee6762593ef14ab459858b'
