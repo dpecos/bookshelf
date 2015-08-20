3d8fbdbab1f893d8c3241dac8628d887af2a3ac0
Daniel Pecos Martinez <contact@danielpecos.com>
2015-08-20 08:42:41 +0200

    Sorting for categories and collections

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -9,7 +9,7 @@ class CategoriesController < ApplicationController
   # GET /categories
   # GET /categories.json
   def index
-    @categories = Category.all
+    @categories = Category.order('name DESC')
 
     respond_to do |format|
       format.html # index.html.erb
--- app/controllers/collections_controller.rb
+++ app/controllers/collections_controller.rb
@@ -2,7 +2,7 @@ class CollectionsController < ApplicationController
   # GET /collections
   # GET /collections.json
   def index
-    @collections = Collection.all
+    @collections = Collection.order('name DESC')
 
     respond_to do |format|
       format.html # index.html.erb
