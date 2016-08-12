5d823e63abf7238c409871a02892d65fc062a596
Daniel Pecos Martinez <contact@danielpecos.com>
2016-08-12 14:08:58 +0200

    Set default route

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,6 +2,7 @@ import { provideRouter, RouterConfig } from '@angular/router';
 import { BookListingComponent, CategoryListingComponent, CollectionListingComponent } from './components';
 
 const routes: RouterConfig = [
+  { path: '', redirectTo: '/books', pathMatch: 'full' },
   { path: 'books', component: BookListingComponent },
   { path: 'categories', component: CategoryListingComponent },
   { path: 'collections', component: CollectionListingComponent }
