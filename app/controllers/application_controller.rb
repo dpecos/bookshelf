af78d3225202ea663ab03cd46a17fadfa4a8ad45
Daniel Pecos <me@danielpecos.com>
2018-09-20 22:49:46 +0200

    Basic HTTP Auth based on ENV var

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,14 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery
+
+  before_filter :http_basic_auth
+
+  def http_basic_auth
+    if ENV['HTTP_AUTH'] =~ %r{(.+)\:(.+)}
+      unless authenticate_with_http_basic { |user, password|  user == $1 && password == $2 }
+        request_http_basic_authentication
+      end
+    end
+  end
+  
 end
