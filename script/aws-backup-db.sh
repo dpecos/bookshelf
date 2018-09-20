5c1e2e37d9a340a2f30c3a813243daf1f6cbd60c
Daniel Pecos <me@danielpecos.com>
2018-09-20 23:05:29 +0200

    Fix: backup script

--- script/aws-backup-db.sh
+++ script/aws-backup-db.sh
@@ -1,7 +1,7 @@
 FILE=bookshelf_$(date +%Y%m%d_%H%M%S).sql.gz
 
 echo "Dumping database..."
-docker exec -it bookshelf_db_1 mysqldump -ubookshelf -pbookshelf bookshelf | gzip > /tmp/$FILE
+docker exec bookshelf_db_1 mysqldump -ubookshelf -pbookshelf bookshelf | gzip > /tmp/$FILE
 
 echo "Copying dump to s3..."
 #s3put --region eu-central-1 -b danielpecos-backups -p apps-bookshelf /tmp/$FILE
