22c0e61595d807ffd0ec6d14a0fd26835cea08db
Daniel Pecos Martinez <me@danielpecos.com>
2020-08-20 16:11:14 +0200

    Frontend: book details

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,7 +1,9 @@
 import React, { Component } from 'react';
 import {
+  Button,
   Col,
   Container,
+  Modal,
   Row,
   Table
   } from 'react-bootstrap';
@@ -11,6 +13,7 @@ interface IProps {}
 
 interface IState {
   books: any[];
+  book: any;
 }
 
 class App extends Component<IProps, IState> {
@@ -19,6 +22,7 @@ class App extends Component<IProps, IState> {
 
     this.state = {
       books: [],
+      book: null,
     };
   }
 
@@ -32,6 +36,16 @@ class App extends Component<IProps, IState> {
       .then((json) => this.setState({ books: json }));
   }
 
+  showBookDetails(book: any) {
+    fetch(`http://localhost:8080/api/books/${book.id}`)
+      .then((response) => response.json())
+      .then((json) => this.setState({ book: json }));
+  }
+
+  hideBookDetails() {
+    this.setState({ book: null });
+  }
+
   render() {
     return (
       <div className="App">
@@ -41,6 +55,7 @@ class App extends Component<IProps, IState> {
               <Table striped bordered hover>
                 <thead>
                   <tr>
+                    <th>Cover</th>
                     <th>Title</th>
                     <th>Author</th>
                     <th>Year</th>
@@ -51,11 +66,21 @@ class App extends Component<IProps, IState> {
                 <tbody>
                   {this.state.books.length === 0 ? (
                     <tr>
-                      <td colSpan={5}>Loading books...</td>
+                      <td colSpan={6}>Loading books...</td>
                     </tr>
                   ) : (
                     this.state.books.map((book) => (
-                      <tr key={book.id}>
+                      <tr
+                        key={book.id}
+                        onClick={() => this.showBookDetails(book)}
+                      >
+                        <td>
+                          <img
+                            alt={book.title}
+                            src={`http://localhost:8080/api/books/${book.id}/cover`}
+                            width="50px"
+                          />
+                        </td>
                         <td>{book.title}</td>
                         <td>{book.author}</td>
                         <td>{book.year}</td>
@@ -66,6 +91,112 @@ class App extends Component<IProps, IState> {
                   )}
                 </tbody>
               </Table>
+
+              <Modal
+                show={this.state.book !== null}
+                animation={false}
+                size="xl"
+                aria-labelledby="contained-modal-title-vcenter"
+                centered
+                onHide={() => {
+                  this.hideBookDetails();
+                }}
+              >
+                <Modal.Header closeButton>
+                  <Modal.Title id="contained-modal-title-vcenter">
+                    {this.state.book?.title}
+                  </Modal.Title>
+                </Modal.Header>
+                <Modal.Body>
+                  <Container>
+                    <Row>
+                      <Col lg="6">
+                        <Table striped bordered hover>
+                          <tbody>
+                            <tr>
+                              <td>Title</td>
+                              <td>{this.state.book?.title}</td>
+                            </tr>
+                            <tr>
+                              <td>Language</td>
+                              <td>{this.state.book?.language}</td>
+                            </tr>
+                            <tr>
+                              <td>Title OV</td>
+                              <td>{this.state.book?.titleOV}</td>
+                            </tr>
+                            <tr>
+                              <td>Languag OV</td>
+                              <td>{this.state.book?.languageOV}</td>
+                            </tr>
+                            <tr>
+                              <td>Author</td>
+                              <td>{this.state.book?.author}</td>
+                            </tr>
+                            <tr>
+                              <td>Year</td>
+                              <td>{this.state.book?.year}</td>
+                            </tr>
+                            <tr>
+                              <td>Category</td>
+                              <td>{this.state.book?.category.name}</td>
+                            </tr>
+                            <tr>
+                              <td>Collection</td>
+                              <td>{this.state.book?.collection?.name}</td>
+                            </tr>
+                            <tr>
+                              <td>Pages</td>
+                              <td>{this.state.book?.pages}</td>
+                            </tr>
+                            <tr>
+                              <td>Editorial</td>
+                              <td>{this.state.book?.editorial}</td>
+                            </tr>
+                            <tr>
+                              <td>ISBN</td>
+                              <td>{this.state.book?.isbn}</td>
+                            </tr>
+                            <tr>
+                              <td>URL</td>
+                              <td>{this.state.book?.url}</td>
+                            </tr>
+                            <tr>
+                              <td>Reading Dates</td>
+                              <td>
+                                {this.state.book?.readingDates.join(' -- ')}
+                              </td>
+                            </tr>
+                          </tbody>
+                        </Table>
+                      </Col>
+                      <Col lg="6">
+                        <img
+                          alt={this.state.book?.title}
+                          src={`http://localhost:8080/api/books/${this.state.book?.id}/cover`}
+                          width="350px"
+                        />
+                      </Col>
+                    </Row>
+                    <Row>
+                      <Col lg={12}>{this.state.book?.abstract}</Col>
+                    </Row>
+                  </Container>
+                </Modal.Body>
+                <Modal.Footer>
+                  <Button
+                    variant="secondary"
+                    onClick={() => {
+                      this.hideBookDetails();
+                    }}
+                  >
+                    Close
+                  </Button>
+                  {/* <Button variant="primary" onClick={handleClose}>
+                    Save Changes
+                  </Button> */}
+                </Modal.Footer>
+              </Modal>
             </Col>
           </Row>
         </Container>
